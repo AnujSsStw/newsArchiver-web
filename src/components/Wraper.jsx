781c0866802a76f818a02131ac3898dac781964d
Anuj Singh <anujZzztw@gmail.com>
2022-06-04 15:31:59 +0530

    change the title and img and added the links

--- src/components/Wraper.jsx
+++ src/components/Wraper.jsx
@@ -2,7 +2,9 @@ const Wraper = (news, index) => {
   return (
     <div key={index} className="feed">
       <div className="flex-none lg:w-60 md:w-60 relative">
-        <img src={news.img} alt="news" />
+        <a href={news.link} target="_blank" rel="noopener noreferrer nofollow">
+          <img src={news.img} alt="news" />
+        </a>
       </div>
       <h2>{news.headline}</h2>
     </div>
